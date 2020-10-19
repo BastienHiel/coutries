992ba0638a4fc87379c20cd24dcf40a6ce1f3b5e
Bastien H <bastien.hiel@gmail.com>
2020-10-19 15:59:29 +0200

    added letters and reset components in app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './App.sass';
 
-import Wording from './Wording'; 
-import Riddle from './Riddle'; 
+import { ALPHABET } from './constants'; 
+import Letters from './Letters'; 
+import Reset from './Reset'; 
+import Riddle from './Riddle';
+import Wording from './Wording';  
 
 class App extends React.Component {
   render() {
@@ -10,6 +13,8 @@ class App extends React.Component {
       <div className='app'>
         <Wording />
         <Riddle phrase='test' />
+        <Letters alphabet={ALPHABET} /> 
+        <Reset />
       </div>
     ); 
   }
