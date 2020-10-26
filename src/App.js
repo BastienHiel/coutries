e29f3b9f3f79a12e8109e92d21c13cd0ecdd3c57
Bastien H <bastien.hiel@gmail.com>
2020-10-26 18:08:48 +0100

    handleLetterClick now search for a match in riddleSet

--- src/App.js
+++ src/App.js
@@ -10,9 +10,11 @@ import Riddle from './Riddle';
 import Wording from './Wording';  
 
 class App extends React.Component {
-  state = {   
-    usedLetters: new Set(),
-    goodLetters: new Set(), 
+  state = {
+    matchedLetters: new Set(), 
+    riddle: "",
+    riddleSet: new Set(),
+    usedLetters: new Set()
   }
 
   addToUsedLetters(letter) {
@@ -21,24 +23,53 @@ class App extends React.Component {
     )
   }
 
+  //populate riddle and riddleSet the first time 
+  componentDidMount() {
+    const riddle = this.generateRiddle(); 
+    const riddleSet = this.computeRiddleSet(riddle); 
+    this.setState({
+      riddle, 
+      riddleSet
+    })
+  }
+
   //only function given in the exercise
   computeDisplay(phrase, usedLetters) {  
     return phrase.replace(/\w/g, (letter) => (usedLetters.has(letter) ? letter : '_'));
   }
 
+  computeRiddleSet(riddle) {
+    return new Set([...riddle.replace(/\s+/g, '').split('')]);
+  }
+
+  generateRiddle() {    
+    return COUNTRY[Math.floor(Math.random()*COUNTRY.length)];  
+  }
+
   //arrow function for binding 
   handleLetterClick = (e) => {
     const clickedLetter = e.target.innerHTML; 
-    this.addToUsedLetters(clickedLetter); 
+    this.addToUsedLetters(clickedLetter);
+    this.matchRiddleSet(clickedLetter); 
+  }
+
+  matchRiddleSet(letter) {
+    const { riddleSet } = this.state; 
+    if (riddleSet.has(letter)) {
+      this.setState(
+        (prevState) => ({ matchedLetters: prevState.matchedLetters.add(letter) })
+      )
+    }
   }
 
   render() {
-    const { usedLetters } = this.state; 
+    const { riddle, usedLetters } = this.state; 
+    const phrase = this.computeDisplay(riddle, usedLetters);
     return (
       <div className='app'>
         <Result won={true} />
         <Wording />        
-        <Riddle phrase='test' />
+        <Riddle phrase={phrase} />
         <Lives />
         <Letters 
           alphabet={ALPHABET} 
