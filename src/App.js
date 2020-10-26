6e708b6088d2e321ea61e9cbed09605ca4065025
Bastien H <bastien.hiel@gmail.com>
2020-10-26 17:44:59 +0100

    import of constant countries

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.sass';
 
-import { ALPHABET } from './constants'; 
+import { ALPHABET, COUNTRY } from './constants'; 
 import Letters from './Letters'; 
 import Lives from './Lives'; 
 import Reset from './Reset'; 
